f63a02ee9ca90f2d5f0da3ca5601ea088f3e9e1c
Alberto Lucas <albertolucasdesousa@gmail.com>
2017-03-11 14:26:24 -0300

    CRUD

--- CrudBasico/Properties/Settings.Designer.cs
+++ CrudBasico/Properties/Settings.Designer.cs
@@ -22,15 +22,5 @@ namespace CrudBasico.Properties {
                 return defaultInstance;
             }
         }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=Z460\\SQLEXPRESS;Initial Catalog=CrudBasico;Integrated Security=True")]
-        public string strConexao {
-            get {
-                return ((string)(this["strConexao"]));
-            }
-        }
     }
 }
